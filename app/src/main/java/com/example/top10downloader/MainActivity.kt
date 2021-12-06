3e87b04250c64de02e5371316297a8f19a132a7d
jiradawi <cutiemoon08@yahoo.com>
2021-12-06 15:24:03 +0700

    add parser

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -1,88 +1,66 @@
-package com.example.top10downloader
+package academy.learnprogramming.top10downloader
 
 import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+//import android.support.*
 import android.util.Log
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.lang.Exception
-import java.lang.StringBuilder
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
+import androidx.appcompat.app.AppCompatActivity
+import com.example.top10downloader.R
 import java.net.URL
 
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+
+    override fun toString(): String {
+        return """
+            name = $name
+            artist = $artist
+            releaseDate = $releaseDate
+            imageURL = $imageURL
+        """.trimIndent()
+    }
+}
+
 class MainActivity : AppCompatActivity() {
-	private val TAG = "MainActivity"
+    private val TAG = "MainActivity"
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-		Log.d(TAG, "OnCreate Called")
+        Log.d(TAG, "onCreate called")
         val downloadData = DownloadData()
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
         Log.d(TAG, "onCreate: done")
-	}
+    }
 
-	companion object {
-		private class DownloadData : AsyncTask<String, Void, String>() {
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
             private val TAG = "DownloadData"
 
             override fun onPostExecute(result: String?) {
                 super.onPostExecute(result)
-                Log.d(TAG, "OnPost Execute: Parameter is $result")
+                Log.d(TAG, "onPostExecute: parameter is $result")
             }
 
             override fun doInBackground(vararg url: String?): String {
                 Log.d(TAG, "doInBackground: starts with ${url[0]}")
                 val rssFeed = downloadXML(url[0])
-                if(rssFeed.isEmpty()){
-                    Log.e(TAG, "Do in BG, ERROR downloading")
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error downloading")
                 }
-                return "doInbackground complete"
+                return rssFeed
             }
-		}
 
-        private fun downloadXML(urlPath: String?): String{
-            val xmlResult = StringBuilder();
-            val TAG = "Download XML"
-            try{
-                val url = URL(urlPath)
-                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-                val response = connection.responseCode
-                Log.d(TAG, "Doenload:Response code was $response")
-
-//            val inputStream = connection.inputStream
-//            val inputStreamReader = InputStreamReader(inputStream)
-//            val reader = BufferedReader(inputStreamReader)
-                val reader = BufferedReader(InputStreamReader(connection.inputStream))
-
-                val inputBuffer = CharArray(500)
-                var charsRead = 0
-                while(charsRead <= 0){
-                    charsRead = reader.read(inputBuffer)
-                    if(charsRead > 0){
-                        xmlResult.append(String(inputBuffer, 0, charsRead))
-                    }
-                }
-                reader.close()
-
-                Log.d(TAG, "Receivd ${xmlResult.length} bytes")
-                return xmlResult.toString()
-            }catch (e: MalformedURLException){
-                Log.e(TAG, "download: invalid URL ${e.message}")
-            }catch (e: IOException){
-                Log.e(TAG, "download: IO Exception reading data ${e.message}")
-            }catch(e: Exception){
-                Log.e(TAG, "UNKNOWN Error: ${e.message}")
+            private fun downloadXML(urlPath: String?): String {
+                return URL(urlPath).readText()
             }
-            return ""
         }
-
-	}
-
+    }
 
 
 }
